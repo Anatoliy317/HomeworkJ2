4eb1face8e6ff77218d37299d8cca597fc53227d
anatoliy.aleshin <anaitolya@mail.ru>
2024-02-22 00:20:27 +0300

    J2

--- src/test/java/tests/RegistrationForm.java
+++ src/test/java/tests/RegistrationForm.java
@@ -1,12 +1,17 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import config.ProjectConfig;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 
+import java.util.Map;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
@@ -14,9 +19,31 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class RegistrationForm extends TestBase {
+public class RegistrationForm  {
 
-    @AfterEach
+    @BeforeAll
+    static void beforeAll() {
+
+        System.setProperty("environment", System.getProperty("environment", "prod"));
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.timeout = 10000;
+        Configuration.pageLoadStrategy = "normal";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = System.getProperty("browserRemoteUrl");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+        @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
